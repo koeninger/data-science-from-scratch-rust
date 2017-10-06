4eef3e94c1a116af84bfba8336af8e98152b72ff
cody koeninger <cody@koeninger.org>
2017-10-06 12:04:40 -0500

    remove useless clone

--- src/statistics.rs
+++ src/statistics.rs
@@ -3,15 +3,9 @@ use num::cast::FromPrimitive;
 use std::cmp::Ordering;
 
 fn mean<T: Num + Copy + FromPrimitive>(v: &[T]) -> Option<T> {
-    let sum = v.iter().fold(T::zero(), |a, b| a + b.clone());
+    let sum = v.iter().fold(T::zero(), |a, b| a + *b);
     // can this meaningfully fail?
-    T::from_usize(v.len()).map(|count| {
-        if count == T::zero() {
-            sum
-        } else {
-            sum / count
-        }
-    })
+    T::from_usize(v.len()).map(|count| if count == T::zero() { sum } else { sum / count })
 }
 
 // sorts v
@@ -22,16 +16,20 @@ fn median<T: Num + Copy + FromPrimitive + PartialOrd>(v: &mut [T]) -> Option<T> 
     } else {
         let mid = n / 2;
         let mut found_nan = false;
-        v.sort_by(|a, b| a.partial_cmp(b).unwrap_or_else(||{
-            found_nan = true;
-            Ordering::Equal
-        }));
+        v.sort_by(|a, b| {
+            a.partial_cmp(b).unwrap_or_else(|| {
+                found_nan = true;
+                Ordering::Equal
+            })
+        });
         if found_nan {
             None
         } else if n % 2 == 1 {
             Some(v[mid])
         } else {
-            Some((v[mid] + v[mid - 1]) / T::from_i32(2).expect("divisible by 2"))
+            Some(
+                (v[mid] + v[mid - 1]) / T::from_i32(2).expect("divisible by 2"),
+            )
         }
     }
 }
@@ -44,8 +42,8 @@ mod tests {
     #[test]
     fn test_mean() {
         assert_eq!(mean(&vec![]), Some(0));
-        assert_eq!(mean(&vec![0,1,2,3]), Some(6 / 4));
-        assert_eq!(mean(&vec![0.0,1.0,2.0,3.0,4.0]), Some(10.0 / 5.0));
+        assert_eq!(mean(&vec![0, 1, 2, 3]), Some(6 / 4));
+        assert_eq!(mean(&vec![0.0, 1.0, 2.0, 3.0, 4.0]), Some(10.0 / 5.0));
     }
 
     #[test]
